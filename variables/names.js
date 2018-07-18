62297601a56987ab41a497d784c32b91b880a776
Lukas Sandman <41347531+sandmanlukas@users.noreply.github.com>
2018-07-18 15:39:15 +0200

    added some folders to help organize things

---
+++ variables/names.js
@@ -0,0 +1,3 @@
+let ourPlanet = "earth";
+
+let currentUserName = "John";
---
+++ variables/vars.js
@@ -0,0 +1,21 @@
+<!DOCTYPE HTML>
+
+<html>
+
+<body>
+
+  <script>
+  "use strict";
+
+  let admin;
+  let name;
+
+  name = "John";
+  admin = name;
+
+  alert(admin);
+
+  </script>
+</body>
+
+</head>
